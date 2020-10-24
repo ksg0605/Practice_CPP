ce8f1c9f3c2bbf103ba45680ccfdca61e67aa9e4
sunkyu Kim <yyh2337@naver.com>
2020-10-24 19:28:34 +0900

    20201024

--- DAY_1/1-4.cpp
+++ DAY_1/1-4.cpp
@@ -1,10 +1,12 @@
 #include  <iostream>
 int main(){
-  for (int i = 1; i < 5; i++)
+  for (int i = 1; i < 6; i++)
   {
-    std::cout<<"*";
-    std::cout<<"\n";
-    return 0;
+    for (int j = 1; j < i; j++)
+    {
+      std::cout << "*";
+    }
+    std::cout << "\n";
   }
-  
+  return 0;
 }
---
+++ DAY_1/Example_2-1.cpp
@@ -0,0 +1,8 @@
+//Simple.cpp
+#include  <iostream>
+
+int main(){
+  std::cout << "HELLO\n";
+  std::cout <<  "첫번째 맛보기입니다.";
+  return 0;
+}
